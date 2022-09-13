e274cdda756f7ce9aa01aaf57b71797d8420ae52
han-gaeul <gmlakdcjs314@gmail.com>
2022-09-13 17:34:08 +0900

    백준 2789번 문제 파일 수정

--- Baekjoon/0801/유학금지.py
+++ Baekjoon/0801/유학금지.py
@@ -2,8 +2,13 @@
 # 첫째 줄에 알파벳 대문자로 이루어진 단어가 주어진다
 # 단어에서 CAMRIDGE에 포함된 알파벳을 모두 지운 뒤 출력
 
-import sys
+camb = 'CAMBRIDGE'
+word = list(input())
 
-# input = sys.stdin.readline
-sys.stdin = open('유학금지.txt', 'r')
+for i in camb:
+    for j in range(len(word)):
+        if i == word[j]:
+            word[j] = ''
 
+for i in word:
+    print(i, end = '')
