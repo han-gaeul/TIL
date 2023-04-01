ea228b30627347b6678f70f438443609d12a5afe
han-gaeul <gmlakdcjs314@gmail.com>
2023-04-01 17:17:52 +0900

    Oracle SQL문 추가

--- Programmers/SQL/groupyby/진료과별총예약횟수출력하기.sql
+++ Programmers/SQL/groupyby/진료과별총예약횟수출력하기.sql
@@ -18,8 +18,16 @@ APPOINTMENT 테이블에서 2022년 5월에 예약한 환자 수를
 예약한 환자 수를 기준으로 오름차순 정렬한다. 예약한 환자 수가
 같다면 진료과 코드를 기준으로 오름차순 정렬 */
 
+-- MySQL
 SELECT MCDP_CD AS '진료과코드', COUNT(PT_NO) AS '5월예약건수'
 FROM APPOINTMENT
 WHERE APNT_YMD LIKE '2022-05%'
+GROUP BY MCDP_CD
+ORDER BY COUNT(PT_NO), MCDP_CD;
+
+-- Oracle
+SELECT MCDP_CD AS '진료과코드', COUNT(PT_NO) AS '5월예약건수'
+FROM APPOINTMENT
+WHERE TO_CHAR(APNT_YMD, 'mm') = '05'
 GROUP BY MCDP_CD
 ORDER BY COUNT(PT_NO), MCDP_CD;
