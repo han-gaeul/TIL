9ce28fb640018e86ff46102b55b7f9a52d5e03f8
han-gaeul <gmlakdcjs314@gmail.com>
2023-04-07 20:44:42 +0900

    0407 프로그래머스 문제 풀기

---
+++ Programmers/SQL/string,date/자동차대여기록에서장기,단기대여구분하기.sql
@@ -0,0 +1,13 @@
+/* 다음은 어느 자동차 대여 회사의 자동차 대여 기록 정보를 담은 CAR_RENTAL_COMPANY_RENTAL_HISTORY
+테이블이다. 이 테이블은 아래와 같은 구조로 되어있으며, HISTORY_ID, CAR_ID, START_DATE,
+END_DATE는 각각 자동차 대여 기록 ID, 자동차 ID, 대여 시작일, 대여 종료일을 나타낸다
+이 테이블에서 대여 시작일이 2022년 9월에 속하는 대여 기록에 대해서 대여 기간이 30일 이상이면
+'장기 대여', 그렇지 않으면 '단기 대여'로 표시하는 컬럼(컬럼명 : RENT_TYPE)을 추가하여 대여기록을
+출력하는 SQL문을 작성하라. 결과는 대여 기록 ID를 기준으로 내림차순 정렬 */
+
+SELECT HISTORY_ID, CAR_ID, DATE_FORMAT(START_DATE, '%Y-%m-%d') AS START_DATE,
+    DATE_FORMAT(END_DATE, '%Y-%m-%d') AS END_DATE,
+    IF(DATEDIFF(END_DATE, START_DATE) >= 29, '장기 대여', '단기 대여') AS RENT_TYPE
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+WHERE DATE_FORMAT(START_DATE, '%Y-%m') LIKE '2022-09'
+ORDER BY HISTORY_ID DESC;
