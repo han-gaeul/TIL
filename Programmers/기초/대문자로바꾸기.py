abc1a18e979baf8f33dbcedbc9bd05089fd7bd81
han-gaeul <gmlakdcjs314@gmail.com>
2023-06-06 16:49:51 +0900

    0606 프로그래머스 문제 풀이

---
+++ Programmers/기초/대문자로바꾸기.py
@@ -0,0 +1,5 @@
+# 알파벳으로 이루어진 문자열 myString이 주어진다
+# 모든 알파벳을 대문자로 변환하여 return
+
+def solution(myString):
+    return myString.upper()
---
+++ Programmers/기초/소문자로바꾸기.py
@@ -0,0 +1,5 @@
+# 알파벳으로 이루어진 문자열 myString이 주어진다
+# 모든 알파벳을 소문자로 변환하여 return
+
+def solution(myString):
+    return myString.lower()
